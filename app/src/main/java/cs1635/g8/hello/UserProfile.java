7c260f0bcaed09a09ec7d045ca2eedb8857fe32c
DBrown910 <dmb147@pitt.edu>
2017-03-20 11:25:49 -0400

    Project Base files

---
+++ app/src/main/java/cs1635/g8/hello/UserProfile.java
@@ -0,0 +1,13 @@
+package cs1635.g8.hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
