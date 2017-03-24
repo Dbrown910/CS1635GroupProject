2ebd2ab3365ab2498f5ed3b4649bdd8f217261d6
DBrown910 <dmb147@pitt.edu>
2017-03-23 21:14:05 -0400

    Basic User Edit page done. Still need to test pic upload.

--- app/src/main/java/cs1635/g8/hello/HomeScreen.java
+++ app/src/main/java/cs1635/g8/hello/HomeScreen.java
@@ -2,12 +2,27 @@ package cs1635.g8.hello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+
+import java.util.HashMap;
 
 public class HomeScreen extends AppCompatActivity {
+    HashMap<String, String> profile_info = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        Bundle b = new Bundle();
+        b = getIntent().getExtras();
+        String name = b.getString("Name");
+        String cell_num = b.getString("Cell");
+
+        TextView tb1 = (TextView) findViewById(R.id.textView);
+        TextView tb2 = (TextView) findViewById(R.id.textView2);
+
+        tb1.setText(name);
+        tb2.setText(cell_num);
     }
 }
