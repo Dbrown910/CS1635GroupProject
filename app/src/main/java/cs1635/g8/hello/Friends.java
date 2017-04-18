ab3951830ce33270410ec8b7992c62bf49556540
Anthony Tummillo <anthonytummillo@Anthonys-MacBook-Pro.local>
2017-04-18 12:24:22 -0400

    added Friends class

--- app/src/main/java/cs1635/g8/hello/Friends.java
+++ app/src/main/java/cs1635/g8/hello/Friends.java
@@ -1,8 +1,13 @@
 package cs1635.g8.hello;
 
+import java.util.ArrayList;
+
 /**
  * Created by anthonytummillo on 4/18/17.
  */
 
 public class Friends {
+    static public Friends friends = new Friends();
+
+    public ArrayList<User> list;
 }
